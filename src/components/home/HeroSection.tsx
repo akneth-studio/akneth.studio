374da9d95e34e947dd692c5352e0e631855fe300
Katarzyna Pawłowska-Malesa (Akneth Studio) <akneth.studio@gmail.com>
2025-06-19 16:14:36 +0200

    Update 2 files
    - /src/styles/main.scss
    - /src/components/home/HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <section className="hero text-center py-5 px-4">
       <p className="lead fs-1" id='lead-gradient'>
-        Szablony, automatyzacje i indywidualne rozwiązania dla firm i klientów indywidualnych.<br />
+        <b>Szablony, automatyzacje i indywidualne rozwiązania dla firm i klientów indywidualnych.</b><br />
         Elastyczność, uczciwość i szybka realizacja - Twoje potrzeby, mój priorytet.
       </p>
       <CTAButton
